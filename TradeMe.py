c1bb22cfd1ecb72253a6bcca6d4a7f0f249197bd
Yhanli <leeyuhan@qq.com>
2018-12-19 11:30:16 +1300

    Update TradeMe.py

--- TradeMe.py
+++ TradeMe.py
@@ -5,7 +5,7 @@ from datetime import datetime
 A7630C27A7C75C544C366E1202C8428D
 95580253737B7282468767972A0E4AD5
 62603E38A04EDB9B6762C3058A0C6ED3
-'''
+'''##
 time = datetime.strptime('1970-01-01 13:00:00',"%Y-%m-%d %H:%M:%S")
 oauth_callback = 'https://www.website-tm-access.co.nz/trademe-callback'
 oauth_consumer_key = '4F4CD97D22E4867B9346D12344D86F81'
